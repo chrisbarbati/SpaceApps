98964996b4f768fdcbc5dcc3b5f6f384744d04aa
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-10-05 10:33:12 -0400

    Initial commit - backend code

---
+++ backend/SpaceAppsLR2/src/main/java/org/greenalien/spaceappslr2/SpaceAppsLr2Application.java
@@ -0,0 +1,13 @@
+package org.greenalien.spaceappslr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceAppsLr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceAppsLr2Application.class, args);
+	}
+
+}
---
+++ backend/SpaceAppsLR2/src/test/java/org/greenalien/spaceappslr2/SpaceAppsLr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greenalien.spaceappslr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceAppsLr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
