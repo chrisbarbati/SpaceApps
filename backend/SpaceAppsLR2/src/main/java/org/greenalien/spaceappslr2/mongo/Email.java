df4a64e895c3c41b3be4ef5fa323e77c28d95d16
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-10-06 10:26:18 -0400

    Add endpoints for email, improve Mongo integration, add API calls

--- backend/SpaceAppsLR2/src/main/java/org/greenalien/spaceappslr2/mongo/Email.java
+++ backend/SpaceAppsLR2/src/main/java/org/greenalien/spaceappslr2/mongo/Email.java
@@ -3,6 +3,8 @@ package org.greenalien.spaceappslr2.mongo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 /**
  * Model class for an email notification entity to be stored in the database.
  */
@@ -15,9 +17,24 @@ public class Email {
 
         private String emailAddress;
 
+        private LocalDateTime date;
+
+        private int leadTime; //Hours before the event to send the email
+
+        private Double latitude;
+
+        private Double longitude;
+
         public Email() {
         }
 
+        public Email(String emailAddress, LocalDateTime date, int leadTime, Double latitude, Double longitude){
+            setEmailAddress(emailAddress);
+            setDate(date);
+            setLeadTime(leadTime);
+            setLatitude(latitude);
+            setLongitude(longitude);
+        }
 
         public String getId() {
             return id;
@@ -35,4 +52,35 @@ public class Email {
             this.emailAddress = emailAddress;
         }
 
+        public LocalDateTime getDate() {
+            return date;
+        }
+
+        public void setDate(LocalDateTime date) {
+            this.date = date;
+        }
+
+        public int getLeadTime() {
+            return leadTime;
+        }
+
+        public void setLeadTime(int leadTime) {
+            this.leadTime = leadTime;
+        }
+
+        public Double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(Double latitude) {
+            this.latitude = latitude;
+        }
+
+        public Double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(Double longitude) {
+            this.longitude = longitude;
+        }
 }
